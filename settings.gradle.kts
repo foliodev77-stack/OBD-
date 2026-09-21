b8d194f29714b6e01a223b7c43688e18d12a5a6a
foliodev77-stack <foliodev77@gmail.com>
2026-09-21 14:42:56 +0200

    Add JitPack for open-source Kotlin OBD API

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral(); maven("https://jitpack.io") } }
 rootProject.name = "OBDScanner"
 include(":app")
